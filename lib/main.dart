3fdfaa4a92dfccfa15657c5cff1da0f1b27c24e1
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2025-05-10 21:51:40 +0300

    chore: update main.dart with I Am Rich app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors
-              .blueGrey,
-          appBar: AppBar(
-            title: Text(
-              'I Am Rich',
-              style: TextStyle(
-                color: Colors.white,
-                // 👈 змінює колір тексту
-                fontSize: 24,
-                fontWeight: FontWeight
-                    .bold,
-              ),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor:
+            Colors.blueGrey,
+        appBar: AppBar(
+          title: Text(
+            'I Am Rich',
+            style: TextStyle(
+              color: Colors.white,
+              // 👈 змінює колір тексту
+              fontSize: 24,
+              fontWeight:
+                  FontWeight.bold,
             ),
-            backgroundColor: Colors.blueGrey[900],
+          ),
+          backgroundColor:
+              Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBMU-Se1F1uq3SSj5J4FT4f9MkBXiCWkpORA&s',
+            ),
           ),
         ),
-      )
+      ),
+    ),
   );
 }
